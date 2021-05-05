ed0de8bfa9352d3382f124941e85574c005548a7
Dmitri Komarov <soviet_uwu@outlook.com>
2021-05-05 21:56:09 +0300

    Fixed header contained non-existent function

--- elipt_cur.h
+++ elipt_cur.h
@@ -3,6 +3,6 @@
 
 int PointMul(mpz_t p, mpz_t a, mpz_t xP, mpz_t yP, mpz_t n, mpz_t xR, mpz_t yR);
 int PointSum(mpz_t p, mpz_t a, mpz_t xP1, mpz_t yP1, mpz_t xP2, mpz_t yP2, mpz_t xR, mpz_t yR);
-int InPlaceOfEuclidus(mpz_t p, mpz_t x, mpz_t revers);
+void Revers(mpz_t p, mpz_t x, mpz_t revers);
 
 #endif
